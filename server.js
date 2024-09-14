f5b8f6d4d4c8d84c7b0aa68163c85a029c3dd72e
Thiago Costa <thycscosta@gmail.com>
2024-09-13 21:31:16 -0300

    Nenhuma modificacao feita

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import app from './src/app.js'
 const PORT = 3000; // <-- definir porta
 
+
 // Escutar porta
 app.listen(PORT, ()=>{
     console.log(`Servidor rodando no local http://localhost:${PORT}`)
 })
+
