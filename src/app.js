d970967ac4b4eea37f35db609c8db0f93740cdda
Thiago Costa <thycscosta@gmail.com>
2024-09-13 21:33:43 -0300

    Modificacoes feita no codigo line 5-13, 16-38

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
-import express from 'express';  // <-- import express
+import express, { Router } from 'express';  // <-- import express
 const app = express(); // <-- criar uma instancia do express
-app.use(express.json()) // <-- indica para o express ler arquivos JSON
+import * as fs from 'node:fs';
 
-// mock
-const selecoes = [
-    {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Servia', grupo: 'G'},
-]
+app.use(express.json({extended: true})) // <-- indica para o express ler arquivos JSON
+app.use((req, res, next) =>{
+    res.setHeader("Access-Control-Allow-Origin",  "*")
+    res.header(
+        "Access-Control-Allow-Header",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next()
+})
 
+
+const readFile = () =>{
+    const content = fs.readFileSync('./db/db.json', 'utf-8')
+    return JSON.parse(content)
+}
+const writeFile = (content) =>{
+    const updateFile = JSON.stringify(content)
+    fs.writeFileSync('./db/db.json', updateFile, 'utf-8')
+}
+app.post('/', (req, res) => {
+    const {id, selecao, grupo} = req.body
+    const currentContent =  readFile()
+    currentContent.push({id, selecao, grupo})
+    writeFile(currentContent)
+    res.send({id, selecao, grupo})
+})
 // criando rota padrão ou raiz
 app.get('/', (req, res) => {
-    res.send(`Olá, está funcionando!`)
+    res.send(readFile())
 })
 
 app.get('/selecoes', (req, res)=>{
-    res.send(selecoes)
+    res.send(readFile())
 })
 app.get('/test', (req, res)=>{
     res.status(400).send('<h1>Não é possível acessar esse site</h1>')
@@ -24,6 +43,7 @@ app.get('/test', (req, res)=>{
 // Rota criado para postar dados
 app.post('/selecoes',  (req, res)=>{
     selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada com sucesso!')
+    res.status(200).send('Seleção cadastrada com sucesso!')
 })
 export default app // <-- exportando app
+
